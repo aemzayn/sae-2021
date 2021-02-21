34e6e170e74d86019095dd5cab26a1ff8d55f863
aemzayn <ahmad.muslih22@gmail.com>
2021-02-21 19:16:59 +0700

    add lang props to html element in head component

--- src/components/meta/head.js
+++ src/components/meta/head.js
@@ -6,6 +6,7 @@ function Head() {
     <>
       <GoogleFonts href='https://fonts.googleapis.com/css2?family=Inter&family=Montserrat:wght@800&display=swap' />
       <NextHead>
+        <html lang='id' />
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
--- src/pages/_documents.js
+++ src/pages/_documents.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang='id'>
+      <Html>
         <Head>
           <link
             rel='apple-touch-icon'
